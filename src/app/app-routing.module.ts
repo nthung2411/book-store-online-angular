a829cb32495588707caeeba9b30ecb4fae14aa9d
Robert Hung Nguyen <nthung2411@gmail.com>
2021-02-01 08:55:34 +0700

    'init'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BooksComponent } from './books/books.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'books',
+    component: BooksComponent
+  },
+  {
+    path: 'books/:id',
+    component: BookDetailComponent
+  },
+  {
+    path: '',
+    redirectTo: 'books',
+    pathMatch: 'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
